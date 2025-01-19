83a0ce1684ecd134698bb385cae41da1377702bb
luzarait <lcanasq@unal.edu.co>
2025-01-19 09:40:38 -0500

    commit

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -6,6 +6,16 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 
 import pandas as pd
+import re
+
+def procesamiento(line):
+    string1 = r"^(\d+)\s+(\d+)\s+(\d+)([,.])(\d+)([ ,.%]+)(.+)"
+    string2 = r"\s{2,5}"
+
+    d = re.search(string1, line)
+    line = d.group(1) + ';' + d.group(2) + ';' + d.group(3) + '.' + d.group(5) + ';' + d.group(7)
+    line = re.sub(string2, ' ', line)
+    return line 
 
 def pregunta_01():
     """
@@ -18,49 +28,50 @@ def pregunta_01():
     - Las palabras clave deben estar separadas por coma y con un solo
       espacio entre palabra y palabra.
     """
+
+    """
+Escriba el codigo que ejecute la accion solicitada en cada pregunta.
+"""
+
     with open('files/input/clusters_report.txt', 'r') as file:
-        lines = file.readlines()
-
-    lines = lines[4:]
+      dataset = file.readlines()
     
-    clusters = []
-    cantidad_palabras_clave = []
-    porcentaje_palabras_clave = []
-    palabras_clave = []
+    cabecera = dataset[:2]
+    data = dataset[4:]
+    data = [line.replace('\n', "") for line in data]
+    data = [line.strip() for line in data]  
+    list_1 = []
+    i = 0
+    line = ""
 
-    current_keywords = [] 
-
-    for line in lines:
-        line = line.strip()
-        
-        if line[0].isdigit():
-            if current_keywords:
-                palabras_clave.append(", ".join(current_keywords).replace(" ,", ","))
-                current_keywords = []
-
-            parts = line.split()
-            clusters.append(int(parts[0]))
-            cantidad_palabras_clave.append(int(parts[1]))
-            porcentaje_palabras_clave.append(float(parts[2].replace(',', '.')))
-            
-            if len(parts) > 3:
-                current_keywords.extend(parts[3:])
+    while i< len(data):
+        if data[i] != "":
+            line = line +' '+ data[i]
         else:
-            current_keywords.extend(line.split())
+            list_1.append(line)
+            line = ""
+        i += 1
 
-    if current_keywords:
-        palabras_clave.append(", ".join(current_keywords).replace(" ,", ","))
-
-    data = {
-        "cluster": clusters,
-        "cantidad_de_palabras_clave": cantidad_palabras_clave,
-        "porcentaje_de_palabras_clave": porcentaje_palabras_clave,
-        "principales_palabras_clave": palabras_clave,
-    }
-    df = pd.DataFrame(data)
-    df.columns = df.columns.str.lower().str.replace(" ", "_")
+    data = [line.strip() for line in list_1]    
+    data = list(map(procesamiento, data))
+    data = [text.split(';') for text in data]
+    data = [[int(i[0]), int(i[1]), float(i[2]), i[3].replace('.', '')] for i in data]
     
+    cabecera = [line.replace('\n', "") for line in cabecera]
+    cabecera = [line.lower() for line in cabecera]
+    cabecera = [line.strip() for line in cabecera]  
+    
+    patron = r"\s{2,5}"
+    
+    cabecera = [re.sub(patron, ';', line) for line in cabecera]
+    cabecera = '; '.join(cabecera)
+    cabecera = cabecera.split(';') 
+    cabecera = [cabecera[0], cabecera[1] + cabecera[4], cabecera[2] +" "+ cabecera[5], cabecera[3]]
+    cabecera = [line.replace(' ', '_') for line in cabecera]
+    
+    df = pd.DataFrame(data, columns = cabecera)
     return df
-df_resultado = pregunta_01()
-print(df_resultado)
+
+print(pregunta_01())      
+        
 
